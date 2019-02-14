a082acba00709cee411d64ffab5599ce04be1f29
Daniela Gonzalez <d.gonzalezc1@uniandes.edu.co>
2019-02-13 22:26:59 -0500

    ArticuloDTO, EventoDTO, ArticuloResource, EventoResource

---
+++ s4_mascotas-api/src/main/java/co/edu/uniandes/csw/mascotas/dtos/EventoDTO.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mascotas.dtos;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author estudiante
+ */
+public class EventoDTO implements Serializable{
+    
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private UsuarioDTO organizador;
+    
+    public EventoDTO () {
+        
+    }
+    
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the imagen
+     */
+    public String getImagen() {
+        return imagen;
+    }
+
+    /**
+     * @param imagen the imagen to set
+     */
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    
+    /**
+     * @return the organizador
+     */
+    public UsuarioDTO getOrganizador() {
+        return organizador;
+    }
+
+    /**
+     * @param elOrganizador the organizador to set
+     */
+    public void setOrganizador(UsuarioDTO elOrganizador) {
+       this.organizador = elOrganizador;
+    }
+    
+}
