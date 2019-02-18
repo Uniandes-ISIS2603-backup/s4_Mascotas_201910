f8eee50bea012082a98a1403144504aa69ce9628
Sebastián Lemus <sebastian.slc9@gmail.com>
2019-02-18 16:54:31 -0500

    Signatures for resource methods added (RecompensaResource)

--- s4_mascotas-api/src/main/java/co/edu/uniandes/csw/mascotas/resources/RecompensaResource.java
+++ s4_mascotas-api/src/main/java/co/edu/uniandes/csw/mascotas/resources/RecompensaResource.java
@@ -5,7 +5,13 @@
  */
 package co.edu.uniandes.csw.mascotas.resources;
 
+import co.edu.uniandes.csw.mascotas.dtos.RecompensaDTO;
+import java.util.List;
+import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -13,10 +19,51 @@ import javax.ws.rs.Produces;
  *
  * @author Sebastián Lemus Cadena (s.lemus)
  */
-@Path("procesosMascotaExtraviada")
+@Path("recompensas")
 @Produces("application/json")
 @Consumes("application/json")
 public class RecompensaResource {
     
+    private static final Logger LOGGER = Logger.getLogger(RecompensaResource.class.getName());
     
+    /**
+     * Realiza la creación de una recompensa de acuerdo
+     * a la información contenida en el DTO dado
+     * @param r - DTO de la recompensa
+     * @return DTO con la información respectiva
+     */
+    @POST
+    public RecompensaDTO crearRecompensa(RecompensaDTO r){
+        return r;
+    }
+    
+    /**
+     * 
+     * @return Todas las recompensas existentes
+     */
+    @GET
+    public List<RecompensaDTO> darRecompensas(){
+        return null;
+    }
+    
+    /**
+     * 
+     * @param id - id de la recompensa
+     * @return La recompensa correspondiente al id
+     */
+    @GET
+    @Path("{id: \\d+}")
+    public RecompensaDTO darRecompensaPorId(Long id){
+        return null;
+    }
+    
+    /**
+     * Elimina la recompensa asociada al id dado
+     * @param id - id de la recompensa
+     */
+    @DELETE
+    @Path("{id: \\d+}")
+    public void eliminarRecompensaPorId(Long id){
+        
+    }
 }
