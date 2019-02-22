446555e2542a3491f227301ed6616407a96d2d6b
SERGIO CANALES MARTINEZ <s.canales@sis.virtual.uniandes.edu.co>
2019-02-22 17:29:01 -0500

    persistence, tests y dtos que faltaban

---
+++ s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/entities/CalificacionEntity.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mascotas.entities;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import uk.co.jemos.podam.common.PodamExclude;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+/**
+ *
+ * @author s.canales
+ */
+@Entity
+public class CalificacionEntity extends BaseEntity implements Serializable {
+    /**
+     * comentario opcional asociado al proceso de adopción
+     */
+    private String comentario;
+    /**
+     * calificación numérica asociada al proceso de adopción
+     */
+    private Integer calificacion;
+    
+    /**
+     * constructor
+     */
+    public CalificacionEntity(){
+        
+    }
+
+    /**
+     * @return the comentario
+     */
+    public String getComentario() {
+        return comentario;
+    }
+
+    /**
+     * @param comentario the comentario to set
+     */
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    /**
+     * @return the calificacion
+     */
+    public Integer getCalificacion() {
+        return calificacion;
+    }
+
+    /**
+     * @param calificacion the calificacion to set
+     */
+    public void setCalificacion(Integer calificacion) {
+        this.calificacion = calificacion;
+    }
+    
+}
---
+++ s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/MascotaEnAdopcionPersistence.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mascotas.persistence;
+
+
+import co.edu.uniandes.csw.mascotas.entities.MascotaEnAdopcionEntity;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author s.canales
+ */
+@Stateless
+public class MascotaEnAdopcionPersistence {
+    
+     @PersistenceContext(unitName="mascotasPU")
+    protected EntityManager em;
+    
+   public MascotaEnAdopcionEntity create(MascotaEnAdopcionEntity mascotaEnAdopcionEntity){
+       
+       em.persist(mascotaEnAdopcionEntity);
+       return mascotaEnAdopcionEntity;
+   }
+   
+   public MascotaEnAdopcionEntity find(Long mascotaEnAdopcionEntity){
+       
+       return em.find(MascotaEnAdopcionEntity.class, mascotaEnAdopcionEntity);
+   }
+   public List<MascotaEnAdopcionEntity> findAll(){
+       
+       TypedQuery<MascotaEnAdopcionEntity> query = em.createQuery("select u from UsuarioEntity u",MascotaEnAdopcionEntity.class );
+       return query.getResultList();
+   }
+
+}
