9addb1287c2c7d804cd153d7650d4ef9cf9f7dd5
Sebastián Lemus <sebastian.slc9@gmail.com>
2019-02-22 19:21:45 -0500

    New rules added on RecompensaLogic and MascotaExtraviadaLogic

--- s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/ejb/RecompensaLogic.java
+++ s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/ejb/RecompensaLogic.java
@@ -9,6 +9,7 @@ import co.edu.uniandes.csw.mascotas.ejb.MascotaExtraviadaLogic;
 import co.edu.uniandes.csw.mascotas.entities.RecompensaEntity;
 import co.edu.uniandes.csw.mascotas.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.mascotas.persistence.RecompensaPersistence;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.inject.Inject;
 
@@ -26,6 +27,13 @@ public class RecompensaLogic {
     @Inject
     private RecompensaPersistence persistence;
     
+    /**
+     * Realiza la creación de una recompensa de acuerdo 
+     * a los valores de la entidad dada
+     * @param r - Entidad de la recompensa
+     * @return Entidad de la recompensa
+     * @throws Exception 
+     */
     public RecompensaEntity createRecompensa(RecompensaEntity r) throws Exception{
         if (r.getValor() < 0) {
             throw new BusinessLogicException("El valor de la recompensa no puede ser negativo");
@@ -38,5 +46,41 @@ public class RecompensaLogic {
         }
         persistence.create(r);
         return r;
+    }
+    
+    /**
+     * 
+     * @return La lista de todas las recompensas existentes
+     */
+    public List<RecompensaEntity> getRecompensas(){
+        return persistence.findAll();
+    }
+    
+    /**
+     * 
+     * @param id - Id de la recompensa a buscar
+     * @return Recompensa asociada al Id
+     * @throws Exception 
+     */
+    public RecompensaEntity getRecompensa(Long id) throws Exception{
+        RecompensaEntity r = persistence.find(id);
+        if(r == null){
+            throw new BusinessLogicException("La recompensa con id " + id + " no existe");
+        }
+        return r;
+    }
+    
+    public RecompensaEntity updateRecompensa(Long id, RecompensaEntity entity) throws Exception{
+        RecompensaEntity r = getRecompensa(id);
+        if (r.getValor() < 0) {
+            throw new BusinessLogicException("El valor nuevo de la recompensa no es correcto; no puede ser negativo");
+        }
+        if(r.getEstado() != RecompensaEntity.PAGADO || r.getEstado() != RecompensaEntity.PENDIENTE){
+           throw new BusinessLogicException("El estado de la recompensa solo puede ser 'PENDIENTE' o 'PAGADO'");
+        }
+        if(!r.getProcesoMascotaExtraviada().equals(entity.getProcesoMascotaExtraviada())){
+           throw new BusinessLogicException("El proceso de mascota extraviada asociada debe ser el mismo que antes para la recompensa con id " + id);
+        }
+        return persistence.update(r);
     }
 }
