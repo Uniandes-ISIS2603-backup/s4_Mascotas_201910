8e0751cb241f7324a6c2addd70e78ca5df260d85
SERGIO CANALES MARTINEZ <s.canales@sis.virtual.uniandes.edu.co>
2019-02-26 23:22:13 -0500

    calificacion resource

--- s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/ejb/CalificacionLogic.java
+++ s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/ejb/CalificacionLogic.java
@@ -8,6 +8,7 @@ package co.edu.uniandes.csw.mascotas.ejb;
 import co.edu.uniandes.csw.mascotas.entities.CalificacionEntity;
 import co.edu.uniandes.csw.mascotas.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.mascotas.persistence.CalificacionPersistence;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -29,6 +30,38 @@ public class CalificacionLogic {
         }
         calificacion = persistence.create(calificacion);
         return calificacion;        
+    }
+    
+    public List<CalificacionEntity> getCalificaciones(){
+        
+        return persistence.findAll();
+    }
+    
+    public CalificacionEntity getCalificacion(Long id) throws BusinessLogicException{
+        
+        CalificacionEntity e = persistence.find(id);
+        if(e == null) throw new BusinessLogicException("No exist la calificación con id " + id);
+        
+        return e;
+            
+        
+    }
+    
+    public void deleteCalificacion(Long id ) throws BusinessLogicException{
+        
+        CalificacionEntity e = persistence.find(id);
+        if(e == null) throw new BusinessLogicException("No exist la calificación con id " + id);
+        
+        persistence.delete(id);
+    }
+    
+    public CalificacionEntity updateCalificacion(Long id, String comentario, Integer calificacion) throws BusinessLogicException{
+        
+        CalificacionEntity e = persistence.find(id);
+        if(e == null) throw new BusinessLogicException("No exist la calificación con id " + id);
+        e.setCalificacion(calificacion);
+        e.setComentario(comentario);
+        return persistence.update(e);
     }
     
 }
