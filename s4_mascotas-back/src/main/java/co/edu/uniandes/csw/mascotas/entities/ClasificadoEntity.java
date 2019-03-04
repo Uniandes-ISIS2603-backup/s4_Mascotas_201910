e9aa5d4f726a208191ed3472cec141eb27ea9e55
JOHAN ESTEBAN VIVAS SEPULVEDA <je.vivas@sis.virtual.uniandes.edu.co>
2019-03-04 12:20:12 -0500

    Entities y el resto

---
+++ s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/entities/ClasificadoEntity.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mascotas.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Johan E. Vivas Sepulveda (je.vivas) 
+ */
+
+@Entity
+public class ClasificadoEntity extends BaseEntity implements Serializable {
+    
+    private String nombre;
+    
+    private String contenido; 
+    
+    private String enlace;
+    
+    public ClasificadoEntity()
+    {
+        
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the contenido
+     */
+    public String getContenido() {
+        return contenido;
+    }
+
+    /**
+     * @param contenido the contenido to set
+     */
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    /**
+     * @return the enlace
+     */
+    public String getEnlace() {
+        return enlace;
+    }
+
+    /**
+     * @param enlace the enlace to set
+     */
+    public void setEnlace(String enlace) {
+        this.enlace = enlace;
+    }
+}
---
+++ s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/entities/MascotaEncontradaEntity.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mascotas.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Johan E. Vivas Sepulveda (je.vivas)
+ */
+
+@Entity
+public class MascotaEncontradaEntity extends BaseEntity implements Serializable{
+    
+    @Temporal(TemporalType.DATE)
+    private Date fechaInicio;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fechaFin;
+    
+    private String estado;
+    
+    private String ubicacion;
+    
+    private String descripcion;
+    
+    public MascotaEncontradaEntity () {
+        
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the ubicacion
+     */
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    /**
+     * @param ubicacion the ubicacion to set
+     */
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
