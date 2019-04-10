e4b90ab49a5367ec0a9a2b9df1cae7363d76702f
Natalia Sanabria Forero <n.sanabria@uniandes.edu.co>
2019-04-09 22:40:11 -0500

    Documentación persistencia mascotaEncontrada añadida

--- s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/MascotaEncontradaPersistence.java
+++ s4_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/MascotaEncontradaPersistence.java
@@ -23,10 +23,17 @@ public class MascotaEncontradaPersistence {
     
     private static final Logger LOGGER = Logger.getLogger(MascotaEncontradaPersistence.class.getName());
     
-    
+    /**
+     * Contexto de persistencia
+     */
     @PersistenceContext (unitName = "mascotasPU")
     protected EntityManager em;
     
+    /**
+     * Crea un nuevo proceso de mascota encontrada en la base de datos
+     * @param mascotaEncontradaEntity
+     * @return 
+     */
     public MascotaEncontradaEntity create(MascotaEncontradaEntity mascotaEncontradaEntity){
         
         em.persist(mascotaEncontradaEntity);
@@ -34,11 +41,20 @@ public class MascotaEncontradaPersistence {
         return mascotaEncontradaEntity;
     }
     
+    /**
+     * Retorna un proceso de mascota encontrada dado su id
+     * @param mascotaEncontradaId
+     * @return 
+     */
     public MascotaEncontradaEntity find(Long mascotaEncontradaId){
         
         return em.find(MascotaEncontradaEntity.class, mascotaEncontradaId);
     }
     
+    /**
+     * Retorna una lista con todos los procesos de mascotas encontradas
+     * @return 
+     */
     public List<MascotaEncontradaEntity> findAll(){
         
         TypedQuery<MascotaEncontradaEntity> query = em.createQuery("select u from MascotaEncontradaEntity u", MascotaEncontradaEntity.class);
@@ -46,12 +62,21 @@ public class MascotaEncontradaPersistence {
         return query.getResultList();
     }
     
+    /**
+     * Elimina el registro de una mascota encontrada de la base de datos
+     * @param id 
+     */
     public void delete(Long id){
         MascotaEncontradaEntity entity = em.find(MascotaEncontradaEntity.class, id);
         
         em.remove(entity);
     }
     
+    /**
+     * Actualiza el registro de una mascota encontrada
+     * @param mascota
+     * @return 
+     */
     public MascotaEncontradaEntity update( MascotaEncontradaEntity mascota ){
         
         return em.merge(mascota);
